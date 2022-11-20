f0744addb1b9c02e4e6b9b7e6b69ff04784df29e
Yuqin Zhao <yz4131@columbia.edu>
2022-11-20 18:48:08 -0500

    latest

--- Frontend/JS/Handler.js
+++ Frontend/JS/Handler.js
@@ -1,9 +1,11 @@
 function errorHandler() {
     const errorMsg = document.getElementsByClassName("errorMsg");
     errorMsg[0].innerHTML = "Invalid Number";
+    errorMsg[0].style.color = "red";
 }
 
 function successHandler() {
     const errorMsg = document.getElementsByClassName("errorMsg");
     errorMsg[0].innerHTML = "Success";
+    errorMsg[0].style.color = "green";
 }
